6f9684d9ace0517d7d695a30e0a09fe12556c826
Regina <khabarova.01@inbox.ru>
2022-12-15 22:02:24 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="calculator">
+        <div className="display">
+            <span>(0)</span> 0
+        </div>
+
+         <div className="operators">
+             <button>x</button>
+             <button>y</button>
+             <button>z</button>
+             <button>w</button>
+
+             <button>DEL</button>
+         </div>
+
+         <div className="digits">
+             <button>&#8743;</button>{/*and*/}
+             <button>&#8744;</button>{/*or*/}
+             <button>&#172;</button>{/*not*/}
+             <button>&#8739;</button>{/*штрих Шеффера*/}
+             <button>&#8595;</button>{/*Стрелка Пирса*/}
+             <button>&#8853;</button>{/*XOR*/}
+             <button>&#8594;</button>{/*Импликация*/}
+             <button>&#8592;</button>{/*Обратная Импликация*/}
+             <button>&#10231;</button>{/*Эквивалентность*/}
+             <button>(</button>
+             <button>)</button>
+             <button>=</button>
+         </div>
+     </div>
     </div>
   );
 }
